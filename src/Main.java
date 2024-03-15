eebaed5d8b1285ce5d4b95d6329b6839e3deb4a8
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-03-15 11:18:25 +0300

    add try and catch

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,26 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //Palindrome for an Integer
-        int input = 151;
-        String reversedInput = "";
-        String stringNumber = Integer.toString(input);
+        try {
+            // Palindrome for an Integer
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter the Integer to check");
+            int input = scanner.nextInt();
+            String reversedInput = "";
+            String stringNumber = Integer.toString(input);
 
+            for (int i = stringNumber.length() - 1; i >= 0; i--) {
+                reversedInput = reversedInput + stringNumber.charAt(i);
+            }
 
-        for(int i = stringNumber.length() - 1; i >=0; i--){
-            reversedInput = reversedInput + stringNumber.charAt(i);
+            if (stringNumber.equals(reversedInput)) {
+                System.out.println("It is a palindrome");
+            } else {
+                System.out.println("It is not a palindrome");
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
         }
-
-        if (stringNumber.equals(reversedInput)){
-            System.out.println("It is a palindrome");
-        } else{
-            System.out.println("It is not a palindrome");
-        }
-
-
-
     }
 }
